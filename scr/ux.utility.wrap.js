74c05eb1c988ba16aa16a2c5e7fef3eeec86fa1f
sw4 <sw4@users.noreply.github.com>
2015-02-20 16:43:47 +0000

    Create ux.utility.wrap.js

---
+++ scr/ux.utility.wrap.js
@@ -0,0 +1,20 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('ux.utility.wrap', [])
+        .factory('uxWrap', ['$templateCache','$compile',function ($templateCache,$compile) {
+            return {
+                wrap:function(scope, el, template){
+
+                    var wrapperEl=document.createElement("div");
+                    wrapperEl.innerHTML=$templateCache.get(template);
+                    var replacedEl=wrapperEl.children[0];
+                    $compile(angular.element(replacedEl))(scope);
+                    var radioEl = el[0].parentNode.replaceChild(replacedEl, el[0]);
+                    replacedEl.insertBefore(radioEl, replacedEl.children[0]);
+
+                }
+            };
+        }]);
+
+})(angular);
