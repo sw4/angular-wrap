2c9d8bf93822e693bf33ac228861b4c1c94db62d
sw4 <sw4@users.noreply.github.com>
2015-02-24 10:43:44 +0000

    Update ux.utility.wrap.js

--- src/ux.utility.wrap.js
+++ src/ux.utility.wrap.js
@@ -9,7 +9,7 @@
                     var replacedEl=wrapperEl.children[0];
                     $compile(angular.element(replacedEl))(scope);
                     var rplEl = el[0].parentNode.replaceChild(replacedEl, el[0]);
-                    replacedEl.insertBefore(rplEl, replacedEl.children[0]);
+                    replacedEl.children.length > 0 ? replacedEl.insertBefore(rplEl, replacedEl.children[0]) : replacedEl.appendChild(rplEl);
                 }
             };
         }]);
